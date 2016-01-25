7248e59a81b2664922bea87b1e46755a1764b522
azization <azization@yahoo.com>
2016-01-26 01:04:47 +0200

    create game

---
+++ js/ActionService.js
@@ -0,0 +1,21 @@
+angular.module('learningGames')
+    .factory('ActionService', ['$http', '$q', function ($http, $q) {
+	var urlPrefix = "http://azization.awardspace.info/Games/";
+	var actionService = {
+		httpGet: function(params){
+			return $http.get(urlPrefix + "php/Actions.php", {params: params});
+		},
+		httpPost: function(params){
+			return $http.post(urlPrefix + "php/Actions.php",  params);
+		},
+		login: function(isNew, user, pwd){
+			return $http.post(urlPrefix + "Login.php",
+				{
+					action: isNew? "new": "login",
+					email: user,
+					pwd: pwd
+				});
+		}
+	};
+	return actionService;
+}]);
