46e0078d449ba8fc86a218c5fe222f48b6579cca
azization <azization@yahoo.com>
2016-01-29 02:43:30 +0200

    creage quest

--- js/ActionService.js
+++ js/ActionService.js
@@ -1,15 +1,29 @@
 angular.module('learningGames')
     .factory('ActionService', ['$http', '$q', function ($http, $q) {
+		
+	var toparams = function(obj) {
+		var p = [];
+		for (var key in obj) {
+			p.push(key + '=' + encodeURIComponent(obj[key]));
+		}
+		return p.join('&');
+	};
 	var actionService = {
 		urlPrefix: "http://azization.awardspace.info/Games/",
 		httpGet: function(params){
-			return $http.get(urlPrefix + "php/Actions.php", {params: params});
+			return $http.get(this.urlPrefix + "php/Actions.php", {params: params});
 		},
 		httpPost: function(params){
-			return $http.post(urlPrefix + "php/Actions.php",  params);
+			//return $http.post(this.urlPrefix + "php/Actions.php",  params);
+			return $http({
+				method: 'POST',
+				url: this.urlPrefix + "php/Actions.php",
+				data: toparams(params),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			});
 		},
 		login: function(isNew, user, pwd){
-			return $http.post(urlPrefix + "Login.php",
+			return $http.post(this.urlPrefix + "Login.php",
 				{
 					action: isNew? "new": "login",
 					email: user,
